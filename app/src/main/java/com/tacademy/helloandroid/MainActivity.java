21398ef16c4b8d618178d318bdc3efdccfed096a
River Pearl <riverpearl0115@gmail.com>
2016-07-11 12:04:23 +0900

    first code

--- app/src/main/java/com/tacademy/helloandroid/MainActivity.java
+++ app/src/main/java/com/tacademy/helloandroid/MainActivity.java
@@ -2,12 +2,32 @@ package com.tacademy.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText inputView;
+    TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputView = (EditText)findViewById(R.id.edit_input);
+        messageView = (TextView)findViewById(R.id.text_message);
+
+        Button btn = (Button)findViewById(R.id.btn_send);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = inputView.getText().toString();
+                messageView.setText(text);
+            }
+        });
     }
 }
