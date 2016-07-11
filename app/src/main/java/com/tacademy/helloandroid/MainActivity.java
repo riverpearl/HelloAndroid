d554e90cbafd170e225873dd76f25eb9d5a9455e
River Pearl <riverpearl0115@gmail.com>
2016-07-11 13:44:39 +0900

    toast example

--- app/src/main/java/com/tacademy/helloandroid/MainActivity.java
+++ app/src/main/java/com/tacademy/helloandroid/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String text = inputView.getText().toString();
                 messageView.setText(text);
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_toast);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String text = getResources().getString(R.string.toast_message);
+                //Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, R.string.toast_message, Toast.LENGTH_SHORT).show(); //위 코드보다 이 형태로 더 많이 쓰임
             }
         });
     }
